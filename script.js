28e98429fd8d83003eef156ee4358576eaa14bd7
Попов Александр Витальевич <popov_av@pik.ru>
2017-02-28 16:37:34 +0300

    Add help

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 ﻿let taskLoaded = false;
 
+function TaskFormatException(message) {
+    this.message = message;
+    if ("captureStackTrace" in Error) {
+        Error.captureStackTrace(this, TaskFormatException);
+    }
+    else {
+        this.stack = (new Error()).stack;
+    }
+}
+TaskFormatException.prototype = Object.create(Error.prototype);
+
+function WorkflowException(message) {
+    this.message = message;
+    if ("captureStackTrace" in Error) {
+        Error.captureStackTrace(this, WorkflowException);
+    }
+    else {
+        this.stack = (new Error()).stack;
+    }
+}
+WorkflowException.prototype = Object.create(Error.prototype);
+
 function generate(content) {
     let task = JSON.parse($("#task-json").text());
     if (!task.notes) {
-        alert("Нужно внести номер протокола в описание (description) задачи");
-        return;
+        throw new TaskFormatException("Нужно внести номер протокола в описание (description) задачи");
     }
-    const subtasks = JSON.parse($("#subtasks-json").text()).map(x => x.name);
+    const subtasks = JSON.parse($("#subtasks-json").text()).map(x => x.name.trim());
+    if (subtasks.length === 0) {
+        throw new TaskFormatException("Список подзадач пуст. Внесите подзадачи или выберите другую задачу.");
+    }
     const nameMatches = /^(\d{2})\.(\d{2})\.(\d{4}) \d{2}:\d{2}\s+(.+)/.exec(task.name);
     if (nameMatches === null) {
-        alert("Неверный формат имени задачи");
-        throw new Error();
+        throw new TaskFormatException("Неверный формат имени задачи");
     }
 
     const zip = new JSZip(content);
     const doc = new Docxtemplater().loadZip(zip);
+    let questions = getQuestions(subtasks);
+    let answers = getAnswers(subtasks);
+    if(questions.length !== answers.length) {
+        throw new TaskFormatException("Не совпадает количество вопросов и ответов");
+    }
     doc.setData({
         protocol_number: task.notes,
         topic: nameMatches[4],
         chairman: val("#chairman"),
         secretary: val("#secretary"),
         participants: val("#participants"),
-        questions: getQuestions(subtasks),
-        answers: getAnswers(subtasks),
+        questions: questions,
+        answers: answers,
         day: nameMatches[1],
         month: nameMatches[2],
         year: nameMatches[3],
@@ -45,8 +73,7 @@ function val(selector) {
 function getAnswers(subtasks) {
     const index = subtasks.indexOf("Решения:");
     if (index === -1) {
-        alert("Должна присутствовать строка 'Решения:'");
-        throw new Error();
+        throw new TaskFormatException("Должна присутствовать строка 'Решения:'");
     }
     const answers = [];
     const answerData = subtasks.slice(index + 1);
@@ -58,7 +85,7 @@ function getAnswers(subtasks) {
             });
         } else {
             if (answers.length === 0) {
-                alert("Первой строкой в блоке Решения должна быть 'По [такой-то] задаче:'");
+                throw new TaskFormatException("Первой строкой в блоке Решения должна быть 'По [такой-то] задаче:'");
             }
             answers[answers.length - 1].subanswers.push({
                 text: answerData[i]
@@ -70,8 +97,7 @@ function getAnswers(subtasks) {
 
 function getQuestions(subtasks) {
     if (subtasks[0] !== "Вопросы:") {
-        alert("Первая строка должна быть 'Вопросы:'");
-        throw new Error();
+        throw new TaskFormatException("Первая строка должна быть 'Вопросы:'");
     }
     const questions = [];
     for (let i = 1; i < subtasks.length; i++) {
@@ -83,19 +109,16 @@ function getQuestions(subtasks) {
             });
         }
     }
-    alert("Должна присутствовать строка 'Решения:'");
-    throw new Error();
+    throw new TaskFormatException("Должна присутствовать строка 'Решения:'");
 }
 
-function click() {
+function onSubmit() {
     if (!taskLoaded) {
-        alert("Нужно загрузить задачу из Asana");
-        return;
+        throw new WorkflowException("Нужно загрузить задачу из Asana");
     }
     let file = $("#file-input").get(0).files[0];
     if (!file) {
-        alert("Нужно выбрать файл");
-        return;
+        throw new WorkflowException("Нужно выбрать файл шаблона протокола");
     }
     const reader = new FileReader();
 
@@ -107,15 +130,13 @@ function click() {
 
 function loadTask() {
     let apikey = "0/cc880fedbaec6446c336f3178bbce1bf"; //$("#apikey").val();
-    if(!apikey) {
-        alert("Нужно ввести API ключ");
-        throw new Error();
+    if (!apikey) {
+        throw new WorkflowException("Нужно ввести API ключ");
     }
     taskLoaded = false;
     let taskMatches = /(\d+)$/.exec($("#task_url").val());
     if (!taskMatches) {
-        alert("Нужно указать валидный URL задачи");
-        return;
+        throw new WorkflowException("Нужно указать валидный URL задачи");
     }
     let loadButton = $("#load_task");
     loadButton.removeClass("btn-primary");
@@ -132,29 +153,26 @@ function loadTask() {
 }
 
 function apiError(message, button) {
-    alert("Ошибка API Asana: " + message);
     button.addClass("btn-danger");
+    throw new WorkflowException("Ошибка API Asana: " + message);
 }
 
 function getSigns(c, s, p) {
     const fioRegex = /(^[\wа-яА-Я]*\s+[\wа-яА-Я]\.[\wа-яА-Я]\.)/u;
     let cMatches = fioRegex.exec(c.trim());
     if (!cMatches) {
-        alert("ФИО председателя невалидно, должно быть Фамилия И.О.");
-        throw new Error();
+        throw new WorkflowException("ФИО председателя невалидно, должно быть Фамилия И.О.");
     }
     const names = [cMatches[1]];
     let sMatches = fioRegex.exec(s.trim());
     if (!sMatches) {
-        alert("ФИО секретаря невалидно, должно быть Фамилия И.О.");
-        throw new Error();
+        throw new WorkflowException("ФИО секретаря невалидно, должно быть Фамилия И.О.");
     }
     if (names.indexOf(sMatches[1]) === -1) {
         names.push(sMatches[1]);
     }
-    if(!p.trim() || p.split(",").length === 0) {
-        alert("Заполните имена участников, разделенные запятой");
-        throw new Error();
+    if (!p.trim() || p.split(",").length === 0) {
+        throw new WorkflowException("Заполните имена участников, разделенные запятой");
     }
     p.split(",").map(x => x.trim()).forEach(x => {
         if (names.indexOf(x) === -1) {
@@ -182,6 +200,30 @@ function getPad(len, ch) {
 
 $(() => {
     // $("#apikey").val(localStorage.getItem("apikey"));
-    $("button#generate").click(click);
-    $("button#load_task").click(loadTask);
+
+    $("#json-fiedls-buton").click((e) => {
+        if(!taskLoaded) {
+            e.stopPropagation();
+            alert("Нужно загрузить задачу из Asana");
+        }
+    });
+
+    Zone.current.fork({
+        onHandleError: function (parentZoneDelegate, currentZone, targetZone, e) {
+            if (e instanceof TaskFormatException) {
+                alert(`Ошибка в формате задачи Asana: ${e.message}`);
+            } else if (e instanceof WorkflowException) {
+                alert(`Ошибка: ${e.message}`);
+            } else {
+                alert(`Неожданая ошибка, подробности в консоли: ${e.message}`);
+                throw e;
+            }
+        }
+    }).run(() => {
+        $("button#load_task").click(loadTask);
+        $("form").submit((e) => {
+            e.preventDefault();
+            onSubmit();
+        });
+    });
 });
